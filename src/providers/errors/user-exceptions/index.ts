c4f558d1da93c1c6dbde399909a125d5fd18139e
Natan Lucena <zerogamerptbr@gmail.com>
2024-06-16 14:03:21 -0300

    feat : add user exceptions

--- src/providers/errors/user-exceptions/index.ts
+++ src/providers/errors/user-exceptions/index.ts
@@ -3,3 +3,13 @@ export class UserAlreadyExistsException extends Error {
     super('User already exists');
   }
 }
+export class UserDoesNotExistsException extends Error {
+  constructor() {
+    super('User does not exists');
+  }
+}
+export class InvalidCredentialsException extends Error {
+  constructor() {
+    super('Invalid credentials');
+  }
+}
