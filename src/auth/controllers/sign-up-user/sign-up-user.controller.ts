f265225b16166ec958456b165828bd23b9de80df
Natan Lucena <zerogamerptbr@gmail.com>
2024-06-16 14:03:41 -0300

    refac: add error handling in the sign up user controller

--- src/auth/controllers/sign-up-user/sign-up-user.controller.ts
+++ src/auth/controllers/sign-up-user/sign-up-user.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { SignUpUserDTO } from 'src/auth/dtos/sign-up-user.dto';
 import { SignUpUserService } from 'src/auth/services/sign-up-user/sign-up-user.service';
+import { UserAlreadyExistsException } from 'src/providers/errors/user-exceptions';
 
 @Controller('sign-up')
 export class SignUpUserController {
@@ -8,6 +15,13 @@ export class SignUpUserController {
 
   @Post()
   async handle(@Body() dto: SignUpUserDTO) {
-    return await this.service.execute(dto);
+    try {
+      return await this.service.execute(dto);
+    } catch (e) {
+      if (e instanceof UserAlreadyExistsException) {
+        throw new BadRequestException('User already exists');
+      }
+      throw new InternalServerErrorException(e.message);
+    }
   }
 }
