d7ab9931169bcd920b563cc4c83972f5b4f494e3
Natan Lucena <zerogamerptbr@gmail.com>
2024-06-16 14:03:55 -0300

    feat: add JWT setup

---
+++ src/auth/strategy/jwt-strategy.ts
@@ -0,0 +1,24 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import 'dotenv/config';
+import { UserRepositoryService } from 'src/providers/repositories/user-repository/user-repository.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepositoryService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userRepository.findUserByEmail(payload.email);
+    if (!user.active) {
+      throw new ForbiddenException('User is not active');
+    }
+    delete user.password;
+
+    return user;
+  }
+}
