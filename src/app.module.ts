79ba9294abc2029b0b6d433347587a6dae808131
Zer0Rx <102997123+Natan-Lucena@users.noreply.github.com>
2024-06-20 10:41:09 -0300

    Develop (#10)
    * feat: add get user decorator
    
    * feat: add product repository
    
    * feat: add create product ROUTE
    
    * feat: add products module
    
    * chore: Update Image model schema and migration
    
    * Feat/add route to receive product image (#6)
    
    * build: add @types/multer dependency
    
    * feat: add route to receive product image
    
    * feat: add cron to handle the dbs (#7)
    
    * Feat/add route to receive product image (#8)
    
    * build: add @types/multer dependency
    
    * feat: add route to receive product image
    
    * feat: add cron to handle the dbs (#9)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { RedisModule } from './redis-module/redis-module.module';
 import { ProvidersModule } from './providers/providers.module';
 import { ProductsModule } from './products/products.module';
+import { InfraModule } from './infra/infra.module';
 
 @Module({
-  imports: [AuthModule, RedisModule, ProvidersModule, ProductsModule],
+  imports: [
+    AuthModule,
+    RedisModule,
+    ProvidersModule,
+    ProductsModule,
+    InfraModule,
+  ],
   controllers: [],
   providers: [],
 })
